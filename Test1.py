04d1288efbbe098f1791e7e67b5f6d73edee618f
Dima Shleifman <Dima.Shleifman@dell.com>
2020-02-20 11:52:31 +0200

    Initial commit

---
+++ Test1.py
@@ -0,0 +1,12 @@
+import unittest
+import time
+from selenium import webdriver
+
+
+class Test1(unittest.TestCase):
+    def test_first_selenium_test(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get("https://www.walla.co.il/")
+        time.sleep(5)
+        self.driver.quit()
