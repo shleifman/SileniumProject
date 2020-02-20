e8f27fa807210a6cf42a035c19722cb311577654
Dima Shleifman <Dima.Shleifman@dell.com>
2020-02-20 13:56:45 +0200

    before_separation

--- Test1.py
+++ Test1.py
@@ -3,10 +3,17 @@ import time
 from selenium import webdriver
 
 
-class Test1(unittest.TestCase):
-    def test_first_selenium_test(self):
+class FirstTest(unittest.TestCase):
+    def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
+
+    def test_1(self):
         self.driver.get("https://www.walla.co.il/")
-        time.sleep(22)
+
+    def test_2(self):
+            self.driver.get("https://www.ynet.co.il/")
+
+    def tearDown(self):
+        time.sleep(10)
         self.driver.quit()
